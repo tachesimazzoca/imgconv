a58bf488941bd7d4d38351fc3b1be21de5da8d08
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2012-09-10 20:16:12 +0900

    Format ConvertOption.scala in Scala style

--- src/main/scala/com/mazzoca/imgconv/ConvertOption.scala
+++ src/main/scala/com/mazzoca/imgconv/ConvertOption.scala
@@ -3,10 +3,15 @@ package com.mazzoca.imgconv
 import scala.collection.mutable.HashMap 
 
 class ConvertOption {
-    var formatName:String = null 
-    var copyright:Boolean = false
-    var device:Device = null  
-    var displayWidth:Int = 240  
-    var displayHeight:Int = 320 
-    var params = new HashMap[String, String]
+
+  var formatName: String = null 
+
+  var copyright: Boolean = false
+
+  var device: Device = null  
+
+  var displayWidth: Int = 240  
+  var displayHeight: Int = 320 
+
+  var params = new HashMap[String, String]
 }
