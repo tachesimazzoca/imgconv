554c0607b3236851356016554495ad2ebe7e6643
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2012-09-10 20:34:10 +0900

    Format PluginBroker.scala in Scala style

--- src/main/scala/com/mazzoca/imgconv/plugins/Plugin.scala
+++ src/main/scala/com/mazzoca/imgconv/plugins/Plugin.scala
@@ -3,6 +3,6 @@ package com.mazzoca.imgconv.plugins
 import java.io.{InputStream, OutputStream}
 
 trait Plugin {
-    def execute(input:InputStream, output:OutputStream)
-}
 
+  def execute(input: InputStream, output: OutputStream)
+}
--- src/main/scala/com/mazzoca/imgconv/plugins/PluginBroker.scala
+++ src/main/scala/com/mazzoca/imgconv/plugins/PluginBroker.scala
@@ -1,65 +1,59 @@
 package com.mazzoca.imgconv.plugins
 
-import java.io._
+import java.io.{InputStream, OutputStream, ByteArrayInputStream, ByteArrayOutputStream}
 
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.JavaConversions._
 
 class PluginBroker {
 
-    var plugins:ArrayBuffer[Plugin] = ArrayBuffer[Plugin]()
-    var debug:Boolean = false
+  var plugins: ArrayBuffer[Plugin] = ArrayBuffer[Plugin]()
+  var debug: Boolean = false
 
-    def execute(input:InputStream, output:OutputStream) = {
+  def execute(input: InputStream, output: OutputStream): Unit = {
 
-        var bais:ByteArrayInputStream = null
-        var baos = new ByteArrayOutputStream()
-        var buf:Array[Byte] = new Array[Byte](4096) 
+    var bais: ByteArrayInputStream = null
+    var baos = new ByteArrayOutputStream()
+    var buf: Array[Byte] = new Array[Byte](4096) 
 
-        try {
-            var n:Int = 0
-            while ({n = input.read(buf, 0, 4096); n > 0}) {
-                baos.write(buf, 0, n)
-            }
-            bais = new ByteArrayInputStream(baos.toByteArray())
-            if (this.debug) {
-                println(baos.size + " byte(s) transferred to initial input stream.")
-            }
-            baos = new ByteArrayOutputStream()
-        } catch {
-            case e:Exception => { throw e }
-        }
+    try {
 
-        try {
+      var n: Int = 0
+      while ({n = input.read(buf, 0, 4096); n > 0}) baos.write(buf, 0, n)
 
-            this.plugins.foreach { plugin =>
+      bais = new ByteArrayInputStream(baos.toByteArray())
+      if (this.debug) println(baos.size + " byte(s) transferred to initial input stream.")
+      baos = new ByteArrayOutputStream()
 
-                if (this.debug) {
-                    println("plugin: " + plugin)
-                }
-                plugin.execute(bais, baos)
+    } catch {
+      case (e: Exception) => { throw e }
+    }
 
-                bais = new ByteArrayInputStream(baos.toByteArray())
-                if (this.debug) {
-                    println(baos.size + " byte(s) transferred to next input stream.")
-                }
-                baos = new ByteArrayOutputStream()
-            }
+    try {
 
-            try {
-                var n:Int = 0
-                while ({n = bais.read(buf, 0, 4096); n > 0}) {
-                    output.write(buf, 0, n)
-                }
-            } catch {
-                case e:Exception => { throw e }
-            }
+      this.plugins.foreach { plugin =>
 
-        } catch {
-            case e:Exception => { throw e }
-        } finally {
-            if (bais != null) bais.close()
-            if (baos != null) baos.close()
-        }
-    } 
+        if (this.debug) println("plugin: " + plugin)
+        plugin.execute(bais, baos)
+
+        bais = new ByteArrayInputStream(baos.toByteArray())
+        if (this.debug) println(baos.size + " byte(s) transferred to next input stream.")
+
+        baos = new ByteArrayOutputStream()
+      }
+
+      try {
+        var n: Int = 0
+        while ({n = bais.read(buf, 0, 4096); n > 0}) output.write(buf, 0, n)
+      } catch {
+        case (e: Exception) => { throw e }
+      }
+
+    } catch {
+      case (e: Exception) => { throw e }
+    } finally {
+      Option(bais) map { _.close() }
+      Option(baos) map { _.close() }
+    }
+  } 
 }
