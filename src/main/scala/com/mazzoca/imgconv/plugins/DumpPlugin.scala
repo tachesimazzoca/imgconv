8b7376d5320648f9ec0b942c84dc33ac20b7de54
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2012-09-11 18:56:55 +0900

    Format DumpPlugin in Scala style
    Use writer: PrintWriter for logging messages

--- src/main/scala/com/mazzoca/imgconv/plugins/DumpPlugin.scala
+++ src/main/scala/com/mazzoca/imgconv/plugins/DumpPlugin.scala
@@ -1,81 +1,92 @@
 package com.mazzoca.imgconv.plugins
 
-import java.io.{InputStream, OutputStream, ByteArrayInputStream, ByteArrayOutputStream, IOException}
+import java.io.{
+  ByteArrayInputStream, ByteArrayOutputStream,
+  InputStream, OutputStream, IOException,
+  PrintWriter
+}
 
-import javax.imageio.{ImageIO, IIOImage, ImageReader}
-import javax.imageio.stream.{ImageInputStream}
-import javax.imageio.metadata.{IIOMetadata}
+import javax.imageio.{IIOImage, ImageIO, ImageReader}
+import javax.imageio.stream.ImageInputStream
+import javax.imageio.metadata.IIOMetadata
 
-import org.w3c.dom.{Node}
+import org.w3c.dom.Node
 
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.JavaConversions._
 
 class DumpPlugin extends Plugin {
 
-    def execute(input:InputStream, output:OutputStream) = {
+  var writer: Option[PrintWriter] = None 
 
-        var baos = new ByteArrayOutputStream()
-        var buf:Array[Byte] = new Array[Byte](4096) 
-        try {
-            var n:Int = 0
-            while ({n = input.read(buf, 0, 4096); n > 0}) {
-                output.write(buf, 0, n)
-                baos.write(buf, 0, n)
-            }
-        } catch {
-            case e:Exception => { throw e }
-        }
+  def execute(input: InputStream, output: OutputStream) = {
 
-        var ir:ImageReader = null
+    var baos = new ByteArrayOutputStream()
 
-        try {
-
-            ImageIO.setUseCache(false)
-
-            val iis:ImageInputStream = ImageIO.createImageInputStream(new ByteArrayInputStream(baos.toByteArray()))
-            Option(ImageIO.getImageReaders(iis)).map { readers =>
-                if (readers.hasNext()) {
-                    ir = readers.next()
-                    ir.setInput(iis)
-                }
-            }
-            if (ir == null) {
-                throw new IOException()
-            }
-
-            var ni:Int = ir.getNumImages(true)
-            for (i <- 0 until ni) {
-                val meta:IIOMetadata = ir.getImageMetadata(i)
-                Option(meta.getMetadataFormatNames()).map {
-                    formatNames => formatNames.foreach { formatName:String =>
-                        this.displayNode(meta.getAsTree(formatName), 0)
-                    }
-                }
-            }
-
-        } catch {
-            case e:Exception => { throw e }
-        } finally {
-            if (ir != null) ir.dispose()
-            if (baos != null) baos.close()
-        }
-    } 
-
-    private def displayNode(node:Node, depth:Int): Unit = {
-        val indent:String = " " * depth
-        println(indent + node.getNodeName())
-        Option(node.getAttributes()).map { attrs =>
-            val c = attrs.getLength()
-            for (i <- 0 until c) {
-                val attr:Node = attrs.item(i)
-                println(indent + " - " + attr.getNodeName() + ": " + attr.getNodeValue())
-            }
-        }
-        var child = node.getFirstChild()
-        while (child != null) {
-            displayNode(child, depth + 1)
-            child = child.getNextSibling()
-        }
+    var buf: Array[Byte] = new Array[Byte](4096) 
+    try {
+      var n: Int = 0
+      while ({n = input.read(buf, 0, 4096); n > 0}) {
+        output.write(buf, 0, n)
+        baos.write(buf, 0, n)
+      }
+    } catch {
+      case (e: Exception) => { throw e }
     }
+
+    var ir: ImageReader = null
+
+    try {
+
+      ImageIO.setUseCache(false)
+
+      val iis: ImageInputStream =
+        ImageIO.createImageInputStream(new ByteArrayInputStream(baos.toByteArray()))
+      Option(ImageIO.getImageReaders(iis)) map { readers =>
+        if (readers.hasNext()) {
+          ir = readers.next()
+          ir.setInput(iis)
+        }
+      }
+      if (ir == null) {
+        throw new IOException()
+      }
+
+      var ni: Int = ir.getNumImages(true)
+      for (i <- 0 until ni) {
+        val meta: IIOMetadata = ir.getImageMetadata(i)
+        Option(meta.getMetadataFormatNames()) map { formatNames =>
+          formatNames.foreach { formatName =>
+            this.displayNode(meta.getAsTree(formatName), 0)
+          }
+        }
+      }
+
+    } catch {
+      case (e: Exception) => { throw e }
+    } finally {
+      Option(ir) map { _.dispose() }
+      Option(baos) map { _.close() }
+    }
+  } 
+
+  private def displayNode(node: Node, depth: Int): Unit = {
+
+    val indent = " " * depth
+
+    this.writer map { _.println(indent + node.getNodeName()) }
+
+    Option(node.getAttributes()) map { attrs =>
+      val c = attrs.getLength()
+      for (i <- 0 until c) {
+        val attr: Node = attrs.item(i)
+        this.writer map { _.println(indent + " - " + attr.getNodeName() + ": " + attr.getNodeValue()) }
+      }
+    }
+    var child = node.getFirstChild()
+    while (child != null) {
+      displayNode(child, depth + 1)
+      child = child.getNextSibling()
+    }
+  }
 }
